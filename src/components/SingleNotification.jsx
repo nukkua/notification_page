4e3de60fd33b7e713473c6080395b74d44d3663b
nukkua <marco.salazar.2014.com@gmail.com>
2023-04-08 19:02:41 -0400

    casi

--- src/components/SingleNotification.jsx
+++ src/components/SingleNotification.jsx
@@ -4,13 +4,16 @@ import "./SingleNotification.css";
 export const SingleNotification = (props) => {
   const notification = props.notification;
   return (
-    <div className="notification-container">
-      <img src={notification.user_image} />
-      <div className="fleexx">
+    <div
+      class={`notification-container ${notification.is_valid ? "" : "invalid"}`}
+    >
+      <img src={notification.user_image} alt="" />
+      <div class="notification-text">
         <h2>{notification.name}</h2>
-        <span>{notification.action}</span>
+        <span>{notification.action}</span>{" "}
+        {!notification.is_valid && <span className="notification-dot"></span>}
+        <h6>{notification.time_action}</h6>
       </div>
-      <h6>{notification.time_action}</h6>
     </div>
   );
 };
